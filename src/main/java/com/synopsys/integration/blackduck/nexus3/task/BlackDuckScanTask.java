f29162c5d925d556570183f087e40ed95f9da70f
Brian Mandel <bamandel@gmail.com>
2018-08-31 13:22:38 -0400

    Added build.gradle and other tests

--- src/main/java/com/synopsys/integration/blackduck/nexus3/task/BlackDuckScanTask.java
+++ src/main/java/com/synopsys/integration/blackduck/nexus3/task/BlackDuckScanTask.java
@@ -1,15 +1,49 @@
+/**
+ * blackduck-nexus3
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.synopsys.integration.blackduck.nexus3.task;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonatype.nexus.repository.Repository;
 import org.sonatype.nexus.repository.RepositoryTaskSupport;
+import org.sonatype.nexus.repository.browse.BrowseService;
 
 @Named
 public class BlackDuckScanTask extends RepositoryTaskSupport {
+    private static final String TEST_KEY = "BD_Test";
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private BrowseService browseService;
+
+    @Inject
+
+    public BlackDuckScanTask(BrowseService browseService) {
+        this.browseService = browseService;
+    }
 
     @Override
     public String getMessage() {
@@ -25,6 +59,19 @@ public class BlackDuckScanTask extends RepositoryTaskSupport {
     @Override
     protected void execute(final Repository repository) {
         logger.info("Found repository: " + repository.getName());
+        //        StorageTx storageTx = repository.facet(AttributesFacetImpl.class).txSupplier().get();
+        //        QueryOptions queryOptions = new QueryOptions(null, null, null, null, null);
+        //        BrowseResult<Component> foundComponentsResult = browseService.browseComponents(repository, queryOptions);
+        //        List<Component> foundComponents = foundComponentsResult.getResults();
+        //        foundComponents.forEach(component -> {
+        //            boolean switched = true;
+        //            String foundValue = (String) component.attributes().get(TEST_KEY);
+        //            if (StringUtils.isNotBlank(foundValue)) {
+        //                switched = !Boolean.parseBoolean(foundValue);
+        //            }
+        //            component.attributes().set(TEST_KEY, switched);
+        //            storageTx.saveComponent(component);
+        //        });
     }
 
 }
--- src/main/java/com/synopsys/integration/blackduck/nexus3/task/BlackDuckScanTaskDescriptor.java
+++ src/main/java/com/synopsys/integration/blackduck/nexus3/task/BlackDuckScanTaskDescriptor.java
@@ -1,9 +1,33 @@
+/**
+ * blackduck-nexus3
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.synopsys.integration.blackduck.nexus3.task;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.formfields.RepositoryCombobox;
+import org.sonatype.nexus.formfields.StringTextFormField;
 import org.sonatype.nexus.repository.RepositoryTaskSupport;
 import org.sonatype.nexus.scheduling.TaskDescriptorSupport;
 
@@ -15,15 +39,21 @@ public class BlackDuckScanTaskDescriptor extends TaskDescriptorSupport {
 
     public BlackDuckScanTaskDescriptor() {
         super(BLACK_DUCK_SCAN_TASK_ID,
-                BlackDuckScanTask.class,
-                BLACK_DUCK_SCAN_TASK_NAME,
-                VISIBLE,
-                EXPOSED,
-                new RepositoryCombobox(
-                        RepositoryTaskSupport.REPOSITORY_NAME_FIELD_ID,
-                        "Repository",
-                        "Repository for BlackDuck to Scan",
-                        true));
+            BlackDuckScanTask.class,
+            BLACK_DUCK_SCAN_TASK_NAME,
+            VISIBLE,
+            EXPOSED,
+            new RepositoryCombobox(
+                RepositoryTaskSupport.REPOSITORY_NAME_FIELD_ID,
+                "Repository",
+                "Repository for BlackDuck to Scan",
+                true),
+            new StringTextFormField(
+                "componentName",
+                "Component Name",
+                "Name of component to add attributes to",
+                true)
+        );
     }
 
 }
